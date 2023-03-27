2686c86c5d8108aa766e7decf27c4305c01617e1
KCSPreethi <preethik@sahaj.ai>
2023-03-27 12:48:17 +0530

    Change the name of S# bucket

--- main.tf
+++ main.tf
@@ -78,8 +78,8 @@ resource "aws_subnet" "preethi_subnet" {
   }
 
 }
-resource "aws_s3_bucket" "preethi-kcs-bucket" {
-  bucket = "preethi-kcs-bucket"
+resource "aws_s3_bucket" "preethi-tf-test-bucket" {
+  bucket = "preethi-tf-test-bucket"
 
   versioning {
     enabled = true
@@ -92,15 +92,6 @@ resource "aws_s3_bucket" "preethi-kcs-bucket" {
       }
     }
   }
-}
-
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.preethi-kcs-bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 
