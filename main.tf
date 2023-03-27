f1616cbe9376b1ba6fbe291e0bb775233f954fe2
KCSPreethi <preethik@sahaj.ai>
2023-03-27 20:33:29 +0530

    first

--- main.tf
+++ main.tf
@@ -1,96 +1,104 @@
- terraform {
-  required_providers {
+  terraform {
+    required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    source  = "hashicorp/aws"
+    version = "~> 4.16"
     }
+    }
+    required_version = ">= 1.2.0"
   }
-  required_version = ">= 1.2.0"
-}
 
-provider "aws" {
-  region  = "us-east-1"
-}
+    provider "aws" {
+    region  = "us-east-1"
+  }
 
-resource "aws_security_group" "TF-SG" {
-  name        = "security_group_terraform"
-  description = "security_group_terraform"
-  vpc_id      = "vpc-019c09a1a0c5b4f6b"
-  #HTTPS
-  ingress {
+
+    resource "aws_security_group" "TF-SG" {
+    name        = "security_group_terraform"
+    description = "security_group_terraform"
+    vpc_id      = "vpc-019c09a1a0c5b4f6b"
+    #HTTPS
+    ingress {
     description      = "HTTPS"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
+    }
+    ingress {
     description      = "HTTP"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-  ingress {
+    }
+    ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
+    }
 
-  egress {
+    egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
+    }
 
-  tags = {
+    tags = {
     Name = "TF-SG"
-  }
-}
-
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.preethi_subnet.id
-  key_name= "preethi-kcs-key"
-  vpc_security_group_ids = [aws_security_group.TF-SG.id]
-
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Preethi-EC2"
-  }
-
-  }
-
-
-resource "aws_s3_bucket" "gurukul-preethi" {
-  bucket = "gurukul-preethi"
-}
-
-resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.gurukul-preethi.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket = aws_s3_bucket.gurukul-preethi.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
     }
   }
-}
 
 
+    resource "aws_instance" "app_server" {
+    ami           = "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.preethi_subnet.id
+    key_name= "preethi-kcs-key"
+    vpc_security_group_ids = [aws_security_group.TF-SG.id]
 
+    associate_public_ip_address = true
+
+    tags = {
+    Name = "Preethi-EC2"
+    }
+
+  }
+
+
+    resource "aws_s3_bucket" "gurukul-preethi" {
+    bucket = "gurukul-preethi"
+  }
+
+    resource "aws_s3_bucket_versioning" "enabled" {
+    bucket = aws_s3_bucket.gurukul-preethi.id
+    versioning_configuration {
+    status = "Enabled"
+    }
+  }
+    resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.gurukul-preethi.id
+
+    rule {
+    apply_server_side_encryption_by_default {
+    sse_algorithm = "AES256"
+    }
+    }
+  }
+
+    # Move the backend configuration below the S3 bucket resource
+    terraform {
+    backend "s3" {
+    bucket         = "gurukul-preethi"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    }
+  }
